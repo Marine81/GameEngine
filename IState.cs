42d7f6423e8ab034223a4e08968af6fd04fbe368
Marine81 <marinesaveyn14@gmail.com>
2026-03-15 15:15:22 +0100

    etape2.1
    creation interface

---
+++ IState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine_SaveynMarine
+{
+    internal interface IState
+    {
+        public void Enter();
+        public void Exit();
+
+        public void Update(float elapsed_time);
+        public void FixedUpdate(float fixed_elapsed_time);
+
+        public void ProcessInput(ConsoleKeyInfo input);
+
+        public void Render();
+
+    }
+}
