9cebabfeb08d5fcc12f447b1df5ff0508f92d42b
Marine81 <marinesaveyn14@gmail.com>
2026-03-31 19:45:40 +0200

    etape 2.1
    creation classe component

---
+++ Component.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine_SaveynMarine
+{
+    public abstract class Component
+    {
+        private bool _isActive = true;
+        protected GameObject _gameObject;
+
+        public Component(GameObject game_object)
+        {
+            _gameObject = game_object;
+        }
+
+        public void Update(float elapsed_time) { }
+        public void FixedUpdate(float Fixed_elapsed_time) { }
+    }
+}
