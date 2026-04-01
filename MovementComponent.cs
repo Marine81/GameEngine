05b4dbf19470152ddecfb0d4cfc6ee646b4044b2
Marine81 <marinesaveyn14@gmail.com>
2026-04-01 10:55:54 +0200

    etape 3.2
    creation movementComponent et positionComponent

---
+++ MovementComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine_SaveynMarine
+{
+    public class MovementComponent
+    {
+        private PositionComponent _positionComponent = new PositionComponent();
+        public MovementComponent(PositionComponent position_component) 
+        {
+            _positionComponent = position_component;
+        }
+    }
+}
